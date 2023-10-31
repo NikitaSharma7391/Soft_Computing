1fefcfe6452b5cd5dc553b884c72ff28e5232453
NikitaSharma7391 <96782426+NikitaSharma7391@users.noreply.github.com>
2023-10-31 18:51:58 +0530

    Add files via upload

---
+++ Assignment03/back_propagation_or.m
@@ -0,0 +1,58 @@
+﻿% Define the OR gate truth table
+X = [0 0; 0 1; 1 0; 1 1]; % Input data
+Y = [0; 1; 1; 1];         % Target output
+
+% Initialize weights and biases
+inputSize = 2;
+hiddenSize = 4;
+outputSize = 1;
+
+% Randomly initialize weights and biases
+W1 = randn(inputSize, hiddenSize);
+b1 = zeros(1, hiddenSize);
+W2 = randn(hiddenSize, outputSize);
+b2 = zeros(1, outputSize);
+
+% Define training parameters
+learningRate = 0.1;
+epochs = 10000;
+
+% Training loop
+for epoch = 1:epochs
+    % Forward pass
+    hiddenInput = X * W1 + b1;
+    hiddenOutput = 1./(1 + exp(-hiddenInput));
+    output = hiddenOutput * W2 + b2;
+    
+    % Compute error
+    error = Y - output;
+    
+    % Backpropagation
+    deltaOutput = error;
+    deltaHidden = deltaOutput * W2' .* hiddenOutput .* (1 - hiddenOutput);
+    
+    % Update weights and biases
+    W2 = W2 + learningRate * hiddenOutput' * deltaOutput;
+    b2 = b2 + learningRate * sum(deltaOutput);
+    W1 = W1 + learningRate * X' * deltaHidden;
+    b1 = b1 + learningRate * sum(deltaHidden);
+end
+
+% Display the final updated weights for each layer
+disp('Final Updated W1:');
+disp(W1);
+disp('Final Updated b1:');
+disp(b1);
+disp('Final Updated W2:');
+disp(W2);
+disp('Final Updated b2:');
+disp(b2);
+
+% Test the trained network
+predictedOutput = round(1./(1 + exp(-(X * W1 + b1))) * W2 + b2);
+
+% Display the results
+disp('Test Input:');
+disp(X);
+disp('Predicted Output:');
+disp(predictedOutput);
---
+++ Assignment03/perceptron_and.m
@@ -0,0 +1,66 @@
+﻿% Step 1: Define the AND gate truth table
+A = [0, 0, 1, 1];
+B = [0, 1, 0, 1];
+Y = [0, 0, 0, 1];
+
+% Step 2: Initialize perceptron weights and bias with random values
+w1 = rand();
+w2 = rand();
+bias = rand();
+
+% Step 3: Define learning rate and number of training epochs
+alpha = 0.1;
+epochs = 1000;
+
+% Step 4: Perceptron training
+for epoch = 1:epochs
+    errors = 0;
+    
+    for i = 1:length(A)
+        % Calculate the weighted sum
+        weighted_sum = A(i) * w1 + B(i) * w2 + bias;
+        
+        % Apply the activation function (Step function)
+        if weighted_sum >= 0
+            prediction = 1;
+        else
+            prediction = 0;
+        end
+        
+        % Calculate the error
+        error = Y(i) - prediction;
+        
+        % Update weights and bias
+        w1 = w1 + alpha * error * A(i);
+        w2 = w2 + alpha * error * B(i);
+        bias = bias + alpha * error;
+        
+        errors = errors + abs(error);
+    end
+    
+    % Check for convergence
+    if errors == 0
+        disp(['Converged after ', num2str(epoch), ' epochs']);
+        break;
+    end
+end
+
+% Display the final weights and bias
+disp('Final Weights:');
+disp(['w1 = ', num2str(w1)]);
+disp(['w2 = ', num2str(w2)]);
+disp(['Bias = ', num2str(bias)]);
+
+% Test the perceptron
+test_inputs = [0, 0; 0, 1; 1, 0; 1, 1];
+disp('Test Results:');
+for i = 1:size(test_inputs, 1)
+    weighted_sum = test_inputs(i, 1) * w1 + test_inputs(i, 2) * w2 + bias;
+    if weighted_sum >= 0
+        prediction = 1;
+    else
+        prediction = 0;
+    end
+    disp(['Input: [', num2str(test_inputs(i, 1)), ', ', num2str(test_inputs(i, 2)), '] => Output: ', num2str(prediction)]);
+end
+
